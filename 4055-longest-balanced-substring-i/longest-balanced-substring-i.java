72f80dcdebb92fb4c21df7a3bc32ee09a41405eb
Thokalaharish8872 <159756302+Thokalaharish8872@users.noreply.github.com>
2026-02-13 01:18:14 +0530

    Time: 150 ms (37.47%) | Memory: 47 MB (64.17%) - LeetSync

--- 4055-longest-balanced-substring-i/longest-balanced-substring-i.java
+++ 4055-longest-balanced-substring-i/longest-balanced-substring-i.java
@@ -1,10 +1,10 @@
 class Solution {
-    private boolean checkStreak(int[] alps, int streak){
+    private boolean checkStreak(int[] freq, int streak){
         
         for(int k = 0; k < 26; k++){
             
-            if(alps[k] != 0 && streak == 0) streak = alps[k];
-            else if(alps[k] != 0 && streak != alps[k]) return false;
+            if(freq[k] != 0 && streak == 0) streak = freq[k];
+            else if(freq[k] != 0 && streak != freq[k]) return false;
         }
 
         return true;
@@ -12,20 +12,21 @@ class Solution {
 
     public int longestBalanced(String s) {
 
-        int n = s.length(), len = 0;
+        int n = s.length(), maxLen = 0;
 
         for(int i = 0; i < n; i++){
-            int[] alps = new int[26];
+
+            int[] freq = new int[26];
             
             for(int j = i; j < n; j++){
-                alps[s.charAt(j) - 'a']++;
+                freq[s.charAt(j) - 'a']++;
 
-                if(checkStreak(alps, 0))
-                    len = Math.max(len, j - i + 1);
+                if(checkStreak(freq, 0))
+                    maxLen = Math.max(maxLen, j - i + 1);
             }
         }
 
-        return len;
+        return maxLen;
     }
         
 }
