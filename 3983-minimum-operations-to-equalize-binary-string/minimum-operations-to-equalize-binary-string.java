86ce1a387eb8d3df28fe0f3a385ffe1144e59219
Thokalaharish8872 <159756302+Thokalaharish8872@users.noreply.github.com>
2026-02-28 02:14:23 +0530

    Time: 556 ms (40.00%) | Memory: 112.9 MB (53.33%) - LeetSync

--- 3983-minimum-operations-to-equalize-binary-string/minimum-operations-to-equalize-binary-string.java
+++ 3983-minimum-operations-to-equalize-binary-string/minimum-operations-to-equalize-binary-string.java
@@ -6,7 +6,7 @@ class Solution {
         Arrays.setAll(ts, i -> new TreeSet<>());
 
         for (int i = 0; i <= n; i++) {
-            ts[i % 2].add(i);
+            ts[i & 1].add(i);
         }
 
         int cnt0 = 0;
@@ -16,28 +16,29 @@ class Solution {
             }
         }
 
-        ts[cnt0 % 2].remove(cnt0);
+        ts[cnt0 & 1].remove(cnt0);
 
         Deque<Integer> q = new ArrayDeque<>();
-        q.offer(cnt0);
+        q.add(cnt0);
 
         int ans = 0;
-        while (!q.isEmpty()) {
-            for (int size = q.size(); size > 0; --size) {
-                int cur = q.poll();
-                if (cur == 0) {
-                    return ans;
-                }
+        while(!q.isEmpty()) {
+            int size = q.size();
+
+            while(size-- != 0) {
+                int cur = q.remove();
+                if (cur == 0) return ans;
 
                 int l = cur + k - 2 * Math.min(cur, k);
                 int r = cur + k - 2 * Math.max(k - n + cur, 0);
 
-                TreeSet<Integer> t = ts[l % 2];
+                TreeSet<Integer> t = ts[l & 1];
 
                 Integer next = t.ceiling(l);
                 while (next != null && next <= r) {
-                    q.offer(next);
+                    q.add(next);
                     t.remove(next);
+
                     next = t.ceiling(l);
                 }
             }
