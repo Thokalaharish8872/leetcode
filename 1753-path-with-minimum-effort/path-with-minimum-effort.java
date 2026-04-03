6bb0e52de92a7b2b37e469bf4ff4dd2cdf7c2564
Thokalaharish8872 <159756302+Thokalaharish8872@users.noreply.github.com>
2026-04-03 10:50:48 +0530

    Time: 56 ms (27.72%) | Memory: 47.6 MB (88.95%) - LeetSync

---
+++ 1753-path-with-minimum-effort/path-with-minimum-effort.java
@@ -0,0 +1,48 @@
+class Solution {
+    public int minimumEffortPath(int[][] heights) {
+
+        int m = heights.length, n = heights[0].length;
+        int[] dir = new int[]{0, 1, 0, -1, 0};
+        int[][] effort = new int[m][n];
+        for(int[] e : effort){
+            Arrays.fill(e, (int)1e9);
+        }
+
+        int minEffort = (int)1e9;
+        
+        PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> {
+            return a[0] - b[0];
+        });
+        effort[0][0] = 0;
+        pq.add(new int[]{0, 0, 0});
+
+        while(!pq.isEmpty()){
+            int[] prev = pq.remove();
+            int e = prev[0];
+            int previ = prev[1];
+            int prevj = prev[2];
+            int maxDiff = 0;
+
+            for(int k = 1; k < 5; k++){
+                int curri = previ + dir[k];
+                int currj = prevj + dir[k - 1];
+
+                if(curri >= 0 && curri < m && currj >=0 && currj < n){
+                    int diff = Math.abs(heights[previ][prevj] - heights[curri][currj]);
+
+                    int newEffort = Math.max(e, diff);
+    
+                    if(newEffort < effort[curri][currj]){
+                        effort[curri][currj] = newEffort;
+
+                        pq.add(new int[]{newEffort, curri, currj});
+                    }
+
+                    if(curri == m - 1 && currj == n - 1) minEffort = Math.min(minEffort, maxDiff);
+                }
+            }
+        }
+
+        return effort[m - 1][n - 1];
+    }
+}
