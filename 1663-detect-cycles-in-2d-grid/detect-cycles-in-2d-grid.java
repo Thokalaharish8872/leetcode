cc7c970f29743e81b359f42cbb982de92764c493
Thokalaharish8872 <159756302+Thokalaharish8872@users.noreply.github.com>
2026-04-26 12:16:29 +0530

    Time: 18 ms (80.48%) | Memory: 140.1 MB (51.25%) - LeetSync

--- 1663-detect-cycles-in-2d-grid/detect-cycles-in-2d-grid.java
+++ 1663-detect-cycles-in-2d-grid/detect-cycles-in-2d-grid.java
@@ -7,29 +7,26 @@ class Solution {
         for(int i = 0; i < m; i++){
             for(int j = 0; j  < n; j++){
                 if(!visited[i][j]){
-                    if(dfs(grid, visited, new boolean[m][n], dir, i, j, -1, -1)) return true;
-                }
+                    if(dfs(grid, visited, dir, i, j, -1, -1)) return true;
+                }   
             }
         }
 
         return false;
     }
 
-    private boolean dfs(char[][] grid, boolean[][] visited, boolean[][] isInCycle, int[] dir, int i, int j, int pi, int pj){
+    private boolean dfs(char[][] grid, boolean[][] visited, int[] dir, int i, int j, int pi, int pj){
         visited[i][j] = true;
-        isInCycle[i][j] = true;
 
         for(int k = 1; k < 5; k++){
             int ni = i + dir[k];
             int nj = j + dir[k - 1];
 
             if(ni >= 0 && nj >= 0 && ni < grid.length && nj < grid[0].length){
-                if(grid[ni][nj] == grid[i][j] && !(pi == ni && pj == nj)){
-                    System.out.println("rotating for " + grid[i][j] + i + j);
-                    if(!isInCycle[ni][nj]){
-                        if(dfs(grid, visited, isInCycle, dir, ni, nj, i, j)) return true;
-                    }
-                    else return true;
+                if(grid[ni][nj] == grid[i][j] && (ni != pi || nj != pj)){
+
+                    if(visited[ni][nj]) return true;
+                    if(dfs(grid, visited, dir, ni, nj, i, j)) return true;
                 }
             }
         }
