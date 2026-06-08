63644105c0b697f61534498c9edc74be592e7fd2
Thokalaharish8872 <159756302+Thokalaharish8872@users.noreply.github.com>
2026-06-08 11:45:10 +0530

    Time: 7 ms (29.43%) | Memory: 159.8 MB (73.16%) - LeetSync

--- 2265-partition-array-according-to-given-pivot/partition-array-according-to-given-pivot.java
+++ 2265-partition-array-according-to-given-pivot/partition-array-according-to-given-pivot.java
@@ -1,30 +1,17 @@
 class Solution {
-    private int add(int[] res, List<Integer> n, int i){
-        for(int num : n){
-            res[i++] = num;
-        }
-        return i;
-    }
     public int[] pivotArray(int[] nums, int pivot) {
-        List<Integer> n1 = new ArrayList<>();
-        List<Integer> n2 = new ArrayList<>();
-        List<Integer> n3  = new ArrayList<>();
-
-        for(int num : nums){
-            if(num < pivot)
-                n1.add(num);
-            else if(num == pivot)
-                n2.add(num);
-            else
-                n3.add(num);
-        }
-
         int n = nums.length;
         int[] res = new int[n];
 
-        int i = add(res, n1, 0);
-        i = add(res, n2, i);
-        add(res, n3, i);
+        int idx = 0;
+        for(int k = 0; k < 3; k++){
+            for(int i = 0; i < n; i++){
+                if((k == 0 && nums[i] < pivot) || 
+                    (k == 1 && nums[i] == pivot) || 
+                        (k == 2 && nums[i] > pivot))
+                    res[idx++] = nums[i];
+            }
+        }
 
         return res;
     }
