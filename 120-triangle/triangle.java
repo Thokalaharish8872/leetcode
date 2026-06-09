3cd6dd6568c90f2066f562f993bdeffa0decccf9
Thokalaharish8872 <159756302+Thokalaharish8872@users.noreply.github.com>
2026-06-09 21:00:33 +0530

    Time: 1 ms (99.88%) | Memory: 45.6 MB (38.86%) - LeetSync

---
+++ 120-triangle/triangle.java
@@ -0,0 +1,32 @@
+class Solution {
+    private int dfs(List<List<Integer>> triangle, int[][] dp, int m, int i, int j){
+        if(i == m)
+            return 0;
+        
+        if(j > i)
+            return Integer.MAX_VALUE;
+        
+        if(dp[i][j] != Integer.MAX_VALUE)
+            return dp[i][j];
+
+        return dp[i][j] = triangle.get(i).get(j) + 
+                            Math.min(
+                                dfs(triangle, dp, m, i + 1, j),
+                                dfs(triangle, dp, m, i + 1, j + 1)
+                            );
+
+    }
+    public int minimumTotal(List<List<Integer>> triangle) {
+        int m = triangle.size();
+
+        int[][] dp = new int[m][];
+
+        for(int i = 0; i < m; i++)
+            dp[i] = new int[triangle.get(i).size()];
+        
+        for(int[] d : dp)
+            Arrays.fill(d, Integer.MAX_VALUE);
+
+        return dfs(triangle, dp, m, 0, 0);
+    }
+}
