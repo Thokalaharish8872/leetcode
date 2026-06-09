8bec1befcf4d7575abfc0fe9b95c87ea520cae8a
Thokalaharish8872 <159756302+Thokalaharish8872@users.noreply.github.com>
2026-06-09 18:27:57 +0530

    Time: 7 ms (10.63%) | Memory: 50.3 MB (37.58%) - LeetSync

--- 64-minimum-path-sum/minimum-path-sum.java
+++ 64-minimum-path-sum/minimum-path-sum.java
@@ -1,26 +1,28 @@
 class Solution {
-    private int dfs(int[][] grid, int[][] minPath, int m, int n, int i, int j){
-        if(i == 0 && j == 0)
-            return grid[i][j];
-        
-        if(i < 0 || j < 0)
-            return Integer.MAX_VALUE;
-
-        if(minPath[i][j] != -1)
-            return minPath[i][j];
-
-        return minPath[i][j] = 
-                Math.min(dfs(grid, minPath, m, n, i - 1, j),
-                        dfs(grid, minPath, m, n, i, j - 1)) + grid[i][j];
-    }
     public int minPathSum(int[][] grid) {
         int m = grid.length, n = grid[0].length;
 
-        int[][] minPath = new int[m][n];
+        int[][] dp = new int[m][n];
 
-        for(int[] path : minPath)
-            Arrays.fill(path, -1);
-        
-        return dfs(grid, minPath, m, n, m - 1, n - 1);
+        for(int i = 0; i < m; i++){
+            for(int j = 0; j < n; j++){
+                if(i == 0 && j == 0){
+                    dp[i][j] = grid[i][j];
+                    continue;
+                }
+
+                dp[i][j] = grid[i][j];
+
+                int up = Integer.MAX_VALUE, left = Integer.MAX_VALUE;
+                if(i > 0)
+                    up = dp[i - 1][j];
+                if(j > 0)                                                                                       
+                    left = dp[i][j - 1];
+                
+                dp[i][j] = Math.min(up, left) + grid[i][j];
+            }
+        }
+
+        return dp[m - 1][n - 1];
     }
 }
