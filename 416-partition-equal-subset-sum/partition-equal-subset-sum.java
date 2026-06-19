1e390e79bec5c9ee98deb027b239f5ddfa63c5a1
Thokalaharish8872 <159756302+Thokalaharish8872@users.noreply.github.com>
2026-06-19 21:44:14 +0530

    Time: 144 ms (5.00%) | Memory: 279.6 MB (5.01%) - LeetSync

---
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.java
@@ -0,0 +1,34 @@
+class Solution {
+    private int f(int[] nums, int[][] dp, int target, int i){
+        if(target == 0)
+            return 1;
+        
+        if(i < 0 || target < 0)
+            return 0;
+    
+        if(dp[i][target] != -1)
+            return dp[i][target];
+        
+        int pick = f(nums, dp, target - nums[i], i - 1);
+        if(pick == 1)
+            return dp[i][target] = 1;
+        
+        return dp[i][target] = f(nums, dp, target, i - 1);
+    }
+    public boolean canPartition(int[] nums) {
+        int n = nums.length;
+        int sum = 0;
+
+        int[][] dp = new int[n][2  * 100_00 + 1];
+
+        for (int i = 0; i < n; i++) {
+            Arrays.fill(dp[i], -1);
+            sum += nums[i];
+        }
+
+        if(sum % 2 == 1)
+            return false;
+
+        return f(nums, dp, sum / 2, n - 1) == 1;
+    }
+}
