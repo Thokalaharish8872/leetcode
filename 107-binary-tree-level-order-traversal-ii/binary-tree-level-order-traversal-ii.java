026c7fd40756cb7a3a3317d96f94f1eba2180e80
I'am single redy to mingle , text me <159756302+Thokalaharish8872@users.noreply.github.com>
2026-07-07 12:26:00 +0530

    Time: 1 ms (96.89%) | Memory: 44.6 MB (40.07%) - LeetSync

---
+++ 107-binary-tree-level-order-traversal-ii/binary-tree-level-order-traversal-ii.java
@@ -0,0 +1,43 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+
+    private void f(TreeNode root, List<List<Integer>> outer, int level){
+        if(root == null)
+            return;
+        
+        if(outer.size() == level){
+            outer.add(0, new ArrayList<>());
+        }
+
+        List<Integer> inner = outer.get(outer.size() - level - 1);
+        inner.add(root.val);
+
+        if(root.left != null)
+            f(root.left, outer, level + 1);
+        
+        if(root.right != null)
+            f(root.right, outer, level + 1);
+
+    }
+    public List<List<Integer>> levelOrderBottom(TreeNode root) {
+        List<List<Integer>> outer = new ArrayList<>();
+        f(root, outer, 0);
+
+        // Collections.reverse(outer);
+        return outer;
+    }
+}
