ebf4e1f04a7440f7573b8cbcb31b030e15b25f06
I'am single redy to mingle , text me <159756302+Thokalaharish8872@users.noreply.github.com>
2026-07-07 12:39:11 +0530

    Time: 2 ms (52.18%) | Memory: 46.3 MB (72.29%) - LeetSync

---
+++ 116-populating-next-right-pointers-in-each-node/populating-next-right-pointers-in-each-node.java
@@ -0,0 +1,54 @@
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public Node left;
+    public Node right;
+    public Node next;
+
+    public Node() {}
+    
+    public Node(int _val) {
+        val = _val;
+    }
+
+    public Node(int _val, Node _left, Node _right, Node _next) {
+        val = _val;
+        left = _left;
+        right = _right;
+        next = _next;
+    }
+};
+*/
+
+class Solution {
+    public Node connect(Node root) {
+        if(root == null)
+            return root;
+            
+        Queue<Node> q = new LinkedList<>();
+        q.add(root);
+
+        while(!q.isEmpty()){
+            int sz = q.size();
+            Node prev = null;
+
+            while(sz-- != 0){
+                Node next = q.poll();
+
+                if(prev != null)
+                    prev.next = next;
+                
+                prev = next;
+
+                if(next.left == null)
+                    continue;
+
+                q.add(next.left);
+                q.add(next.right);
+            }
+        }
+
+        return root;
+    }
+}
