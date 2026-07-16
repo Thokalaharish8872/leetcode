245d217ec20e293e52a5bfc0f9183051b0d0dc6b
I'am single redy to mingle , text me <159756302+Thokalaharish8872@users.noreply.github.com>
2026-07-16 07:50:23 +0530

    Time: 52 ms (99.50%) | Memory: 107.7 MB (93.50%) - LeetSync

---
+++ 4242-sum-of-gcd-of-formed-pairs/sum-of-gcd-of-formed-pairs.java
@@ -0,0 +1,37 @@
+class Solution {
+    private int gcd(int a, int b){
+        while(b > 0){
+            int temp = b;
+            b = a % b;
+            a = temp;
+        }
+        return a;
+    }
+
+    public long gcdSum(int[] nums) {
+        int n = nums.length;
+
+        int[] prefixGcd = new int[n];
+        prefixGcd[0] = nums[0];
+        
+        int max = nums[0];
+
+        for(int i = 1; i < n; i++){
+            max = Math.max(max, nums[i]);
+
+            if(max != nums[i])
+                prefixGcd[i] = gcd(max, nums[i]);
+            else
+                prefixGcd[i] = nums[i];
+        }
+
+        Arrays.sort(prefixGcd);
+
+        long sum = 0;
+
+        for(int i = 0, j = n - 1; i < j; i++, j--)
+            sum += gcd(prefixGcd[i], prefixGcd[j]);
+
+        return sum;
+    }
+}
