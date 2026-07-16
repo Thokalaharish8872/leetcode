7f8a2ee3687d47cb443fc15cb143f98f0037ccb2
I'am single redy to mingle , text me <159756302+Thokalaharish8872@users.noreply.github.com>
2026-07-16 20:33:33 +0530

    Time: 1 ms (83.02%) | Memory: 42.4 MB (74.81%) - LeetSync

--- 3995-gcd-of-odd-and-even-sums/gcd-of-odd-and-even-sums.java
+++ 3995-gcd-of-odd-and-even-sums/gcd-of-odd-and-even-sums.java
@@ -1,28 +1,6 @@
 class Solution {
 
-    private int gcd(int a, int b){
-        while(b > 0){
-            int temp = b;
-            b = a % b;
-            a = temp;
-        }
-
-        return a;
-    }
-
     public int gcdOfOddEvenSums(int n) {
-        
-        int sumOdd = 0;
-        int sumEven = 0;
-
-        do{
-            sumOdd += (2 * n - 1);
-            sumEven += (2 * n);
-
-            System.out.println(sumOdd + " " + sumEven);
-        }
-        while(n-- > 1);
-
-        return gcd(sumOdd, sumEven);
+       return n;
     }
 }
