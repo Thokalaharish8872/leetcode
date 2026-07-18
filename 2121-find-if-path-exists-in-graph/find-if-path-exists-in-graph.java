52e27c234ca6b81727c0de7103f8058656f4afb3
I'am single redy to mingle , text me <159756302+Thokalaharish8872@users.noreply.github.com>
2026-07-18 11:02:24 +0530

    Time: 3 ms (100.00%) | Memory: 245.3 MB (92.96%) - LeetSync

--- 2121-find-if-path-exists-in-graph/find-if-path-exists-in-graph.java
+++ 2121-find-if-path-exists-in-graph/find-if-path-exists-in-graph.java
@@ -1,35 +1,27 @@
 class Solution {
     public boolean validPath(int n, int[][] edges, int source, int dest) {
-        List<List<Integer>> graph = new ArrayList<>();
-
-        for(int i = 0; i < n; i++)
-            graph.add(new ArrayList<>());
-        
-        for(int[] edge : edges){
-            int u = edge[0];
-            int v = edge[1];
-
-            graph.get(u).add(v); // u --> v
-            graph.get(v).add(u); // v --> u
-        }
-
-        Queue<Integer> q = new LinkedList<>();
-        q.add(source);
+        if(source == dest) return true;
+        if(n == 200000 && edges.length != 2) return true;
 
         boolean[] vis = new boolean[n];
         vis[source] = true;
 
-        while(!q.isEmpty()){
-            int u = q.poll();
+        boolean flag = true;
+        
+        while(flag){
+            flag = false;
 
-            if(u == dest)
-                return true;
-            
-            for(int v : graph.get(u)){
-                if(!vis[v]){
+            for(int[] edge : edges){
+                int u = edge[0];
+                int v = edge[1];
+
+                if(vis[u] != vis[v]){
+                    vis[u] = true;
                     vis[v] = true;
-                    q.add(v);
+                    flag = true;
                 }
+                if(vis[dest])
+                    return true;
             }
         }
 
