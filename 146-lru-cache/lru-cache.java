c720a6db39a37afe0e2187cca750f15609f1c85a
Harish Thokala <159756302+Thokalaharish8872@users.noreply.github.com>
2026-07-25 11:06:06 +0530

    Time: 38 ms (99.09%) | Memory: 131.2 MB (61.13%) - LeetSync

--- 146-lru-cache/lru-cache.java
+++ 146-lru-cache/lru-cache.java
@@ -11,76 +11,90 @@ class LRUCache {
             this.val = val;
         }
     }
+
+
     int capacity = 0;
     HashMap<Integer, Node> map;
     Node head, tail;
 
     public LRUCache(int capacity) {
 
+        // fresh cache memory
         map = new HashMap<>();
-
         this.capacity = capacity;
     }
     
     public int get(int key) {
-        Node curr = null;
 
-        if(map.containsKey(key)){
-            curr = map.get(key);
-            update(curr, false);
-        }
+        // if data is not present then return -1;
+        if(!map.containsKey(key)) return -1;
 
-        return curr == null ? -1 : curr.val;
+        // update LRU
+        Node curr = map.get(key);
+        update(curr, false);
+
+        return curr.val;
     }
     
     public void put(int key, int value) {
         Node curr;
 
+        // update the val of key if present and also update LRU
         if(map.containsKey(key)){
             curr = map.get(key);
             curr.val = value;
 
             update(curr, false);
         }
+
+        // if not present add new node in map and also update LRU
         else{
             curr = new Node(key, value);
-            map.put(key, curr);
 
-            if(map.size() > capacity){
+            // before adding into LRU check if capacity of the LRU reached
+            if(map.size() == capacity){
                 map.remove(head.key);
 
+                // if capacity is 1 make everything null, in this way we will overcome null pointer exception
                 if(capacity == 1){
                     head = null;
                     tail = null;
                 }
+
+                // if capacity is not 1 we can perform normal operation i.e remove least recently used data and by removing head
                 else{
                     head = head.next;
                     head.prev = null;
                 }
             }
 
+            // add new data to the LRU and update
+            map.put(key, curr);
             update(curr, true);
         }
     }
 
     private void update(Node curr, boolean isNew){
-        if(isNew){
-            if(tail == null){
-                head = curr;
-                tail = curr;
-            }
-            else{
-                tail.next = curr;
-                curr.prev = tail;
 
-                tail = curr;
-            }
+        // irrespective of new data or update data we make the curr node as head and tail
+        if(head == null && tail == null){
+            head = curr;
+            tail = curr;
+            return;
         }
+
+
+        // perform's this logic if the data is newly added
+        if(isNew){
+
+            tail.next = curr;
+            curr.prev = tail;
+
+            tail = curr;
+        }
+
+        // performs' this logic if the data needs update instead of adding new data
         else{
-            if(head == null){
-                head = curr;
-                tail = curr;
-            }
             if(curr == tail){
                 return;
             }
