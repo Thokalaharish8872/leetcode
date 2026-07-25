cb37e405478616e5375fcaa114272189f30d768c
Harish Thokala <159756302+Thokalaharish8872@users.noreply.github.com>
2026-07-25 11:54:27 +0530

    Time: 47 ms (48.26%) | Memory: 131.6 MB (37.01%) - LeetSync

--- 146-lru-cache/lru-cache.java
+++ 146-lru-cache/lru-cache.java
@@ -1,118 +1,109 @@
 class LRUCache {
+    Map<Integer, Node> map;
+    int capacity;
 
-    class Node{
-        Node next;
-        Node prev;
-        int key;
-        int val;
-
-        Node(int key, int val){
-            this.key = key;
-            this.val = val;
-        }
-    }
-
-
-    int capacity = 0;
-    HashMap<Integer, Node> map;
-    Node head, tail;
+    CDLL cdll;
 
     public LRUCache(int capacity) {
+        cdll = new CDLL();
 
-        // fresh cache memory
         map = new HashMap<>();
         this.capacity = capacity;
     }
     
     public int get(int key) {
-
-        // if data is not present then return -1;
-        if(!map.containsKey(key)) return -1;
-
-        // update LRU
-        Node curr = map.get(key);
-        update(curr, false);
-
-        return curr.val;
+        if(!map.containsKey(key))
+            return -1;
+        
+        return cdll.moveBegin(map.get(key));
     }
     
     public void put(int key, int value) {
-        Node curr;
+        Node node;
 
-        // update the val of key if present and also update LRU
         if(map.containsKey(key)){
-            curr = map.get(key);
-            curr.val = value;
+            node = map.get(key);
+            node.val = value;
 
-            update(curr, false);
+            cdll.moveBegin(node);
         }
-
-        // if not present add new node in map and also update LRU
         else{
-            curr = new Node(key, value);
-
-            // before adding into LRU check if capacity of the LRU reached
+            node = new Node(key, value);
+            
             if(map.size() == capacity){
-                map.remove(head.key);
-
-                // if capacity is 1 make everything null, in this way we will overcome null pointer exception
-                if(capacity == 1){
-                    head = null;
-                    tail = null;
-                }
-
-                // if capacity is not 1 we can perform normal operation i.e remove least recently used data and by removing head
-                else{
-                    head = head.next;
-                    head.prev = null;
-                }
+                Node removed = cdll.removeLast();
+                map.remove(removed.key);
             }
-
-            // add new data to the LRU and update
-            map.put(key, curr);
-            update(curr, true);
+            
+            cdll.addNode(node);
+            map.put(key, node);
         }
     }
+}
 
-    private void update(Node curr, boolean isNew){
+class CDLL{
+    Node head;
+    Map<Integer, Node> map;
 
-        // irrespective of new data or update data we make the curr node as head and tail
-        if(head == null && tail == null){
-            head = curr;
-            tail = curr;
+    int moveBegin(Node node){
+        if(node == head)
+            return node.val;;
+        
+        node.prev.next = node.next;
+        node.next.prev = node.prev;
+        
+        node.next = head;
+        node.prev = head.prev;
+        head.prev.next = node;
+        head.prev = node;
+
+        head = node;
+
+        return node.val;
+    }
+
+    Node removeLast(){
+        Node rem = head;
+
+        if(head.prev == head){
+            head = null;
+            return rem;
+        }
+
+        rem = head.prev;
+
+        head.prev.prev.next = head;
+        head.prev = head.prev.prev;
+
+        return rem;
+    }
+
+    void addNode(Node node){
+        if(head == null){
+            head = node;
+            head.prev = head;
+            head.next = head;
             return;
         }
 
+        head.prev.next = node;
+        node.prev = head.prev;
 
-        // perform's this logic if the data is newly added
-        if(isNew){
+        head.prev = node;
+        node.next = head;
 
-            tail.next = curr;
-            curr.prev = tail;
+        head = node;
+    }
 
-            tail = curr;
-        }
+}
 
-        // performs' this logic if the data needs update instead of adding new data
-        else{
-            if(curr == tail){
-                return;
-            }
-            if(curr == head){
-                head = curr.next;
-                head.prev = null;
-            }
-            else{
-                curr.prev.next = curr.next;
-            }
-            
-            curr.next.prev = curr.prev;
-            curr.next = null;
+class Node{
+    int key, val;
+    Node prev, next;
 
-            tail.next = curr;
-            curr.prev = tail;
-            tail = curr;
-        }
+    Node(int key, int val){
+        this.key = key;
+        this.val = val;
     }
 }
 
