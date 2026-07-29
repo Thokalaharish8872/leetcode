cd7b9abf9d7809fb222d7da8cc2f838b3fa7ef4f
Harish Thokala <159756302+Thokalaharish8872@users.noreply.github.com>
2026-07-30 01:20:33 +0530

    Time: 4 ms (76.34%) | Memory: 44.6 MB (97.33%) - LeetSync

--- 72-edit-distance/edit-distance.java
+++ 72-edit-distance/edit-distance.java
@@ -3,26 +3,59 @@ class Solution {
     public int minDistance(String word1, String word2) {
         int m = word1.length(), n = word2.length();
 
-        int[][] dp = new int[m + 1][n + 1];
+        int[] prev = new int[n + 1];
+        int[] curr = new int[n + 1];
         
-        for(int i = 0; i <= m; i++)
-            dp[i][0] = i;
-        for(int i = 0; i <= n; i++)
-            dp[0][i] = i;
+        for(int j = 0; j <= n; j++)
+            prev[j] = j;
 
         for(int i = 1; i <= m; i++){
+            curr[0] = i;
+
             for(int j = 1; j <= n; j++){
                 if(word1.charAt(i - 1) == word2.charAt(j - 1))
-                    dp[i][j] = dp[i - 1][j - 1];
+                    curr[j] = prev[j - 1];
                 
                 else
-                    dp[i][j] = 1 + Math.min(dp[i - 1][j], Math.min(dp[i][j - 1], dp[i - 1][j - 1]));
+                    curr[j] = 1 + Math.min(curr[j - 1], Math.min(prev[j - 1], prev[j]));
             }
+
+            int[] temp = prev;
+            prev = curr;
+            curr = temp;
         }
 
-        return dp[m][n];
+        return prev[n];
     }
 }
+
+// tabulation
+
+// class Solution {
+    
+//     public int minDistance(String word1, String word2) {
+//         int m = word1.length(), n = word2.length();
+
+//         int[][] dp = new int[m + 1][n + 1];
+        
+//         for(int i = 0; i <= m; i++)
+//             dp[i][0] = i;
+//         for(int i = 0; i <= n; i++)
+//             dp[0][i] = i;
+
+//         for(int i = 1; i <= m; i++){
+//             for(int j = 1; j <= n; j++){
+//                 if(word1.charAt(i - 1) == word2.charAt(j - 1))
+//                     dp[i][j] = dp[i - 1][j - 1];
+                
+//                 else
+//                     dp[i][j] = 1 + Math.min(dp[i - 1][j], Math.min(dp[i][j - 1], dp[i - 1][j - 1]));
+//             }
+//         }
+
+//         return dp[m][n];
+//     }
+// }
 
 // recursion with memoiation
 
