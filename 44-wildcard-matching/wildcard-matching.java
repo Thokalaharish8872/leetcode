fe54faa8f74bd989aebc7a0d176855d2020e9229
Harish Thokala <159756302+Thokalaharish8872@users.noreply.github.com>
2026-08-03 00:13:36 +0530

    Time: 50 ms (33.80%) | Memory: 47.9 MB (27.62%) - LeetSync

--- 44-wildcard-matching/wildcard-matching.java
+++ 44-wildcard-matching/wildcard-matching.java
@@ -1,42 +1,64 @@
 class Solution {
-    private boolean f(String s, String p, int[][] dp, int i, int j){
-        if(i == -1 && j == -1)
-            return true;
-        if(j == -1)
-            return false;
-        
-        if(i == -1){
-            while(j >= 0 && p.charAt(j) == '*')
-                j--;
-            return j == -1;
-        }
-
-        if(dp[i][j] != -1)
-            return dp[i][j] == 1;
-            
-        if(s.charAt(i) == p.charAt(j) || p.charAt(j) == '?'){
-            boolean res = f(s, p, dp, i - 1, j - 1);
-            dp[i][j] =  res ? 1 : 0;
-
-            return res;
-        }
-
-        if(p.charAt(j) == '*'){
-            boolean res = f(s, p, dp, i, j - 1) || f(s, p, dp, i - 1, j);
-            dp[i][j] = res ? 1 : 0;
-            return res;
-        }
-        
-        dp[i][j] = 0;
-        return false;
-    }
     public boolean isMatch(String s, String p) {
         int m = s.length(), n = p.length();
 
-        int[][] dp = new int[m][n];
-        for(int[] d : dp)
-            Arrays.fill(d, -1);
+        boolean[][] dp = new boolean[m + 1][n + 1];
+        dp[0][0] = true;
 
-        return f(s, p, dp, m - 1, n - 1);
+        for(int j = 1; j <= n && p.charAt(j - 1) == '*'; j++)
+            dp[0][j] = true;
+
+        for(int i = 1; i <= m; i++){
+            for(int j = 1; j <= n; j++){
+                if(s.charAt(i - 1) == p.charAt(j - 1) || p.charAt(j - 1) == '?')
+                    dp[i][j] = dp[i - 1][j - 1];
+                else if(p.charAt(j - 1) == '*')
+                    dp[i][j] = dp[i - 1][j] || dp[i][j - 1];
+            }
+        }
+        return dp[m][n];
     }
 }
+
+// class Solution {
+//     private boolean f(String s, String p, int[][] dp, int i, int j){
+//         if(i == -1 && j == -1)
+//             return true;
+//         if(j == -1)
+//             return false;
+        
+//         if(i == -1){
+//             while(j >= 0 && p.charAt(j) == '*')
+//                 j--;
+//             return j == -1;
+//         }
+
+//         if(dp[i][j] != -1)
+//             return dp[i][j] == 1;
+            
+//         if(s.charAt(i) == p.charAt(j) || p.charAt(j) == '?'){
+//             boolean res = f(s, p, dp, i - 1, j - 1);
+//             dp[i][j] =  res ? 1 : 0;
+
+//             return res;
+//         }
+
+//         if(p.charAt(j) == '*'){
+//             boolean res = f(s, p, dp, i, j - 1) || f(s, p, dp, i - 1, j);
+//             dp[i][j] = res ? 1 : 0;
+//             return res;
+//         }
+        
+//         dp[i][j] = 0;
+//         return false;
+//     }
+//     public boolean isMatch(String s, String p) {
+//         int m = s.length(), n = p.length();
+
+//         int[][] dp = new int[m][n];
+//         for(int[] d : dp)
+//             Arrays.fill(d, -1);
+
+//         return f(s, p, dp, m - 1, n - 1);
+//     }
+// }
