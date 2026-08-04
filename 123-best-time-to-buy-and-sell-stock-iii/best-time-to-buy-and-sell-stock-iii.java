a2fa559fc498cd614b8c930f8071bf0f65fe6682
Harish Thokala <159756302+Thokalaharish8872@users.noreply.github.com>
2026-08-04 23:00:58 +0530

    Time: 66 ms (29.92%) | Memory: 134 MB (21.05%) - LeetSync

--- 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.java
+++ 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.java
@@ -1,31 +1,54 @@
 class Solution {
-    private int f(int[] prices, int[][][] dp, int i, int transactions, int buy){
-        if(transactions == -1 || i == prices.length)
-            return 0;
-
-        if(dp[i][transactions][buy] != -1)
-            return dp[i][transactions][buy];
-
-        if(buy == 1)
-            return dp[i][transactions][buy] = Math.max(
-                    -prices[i] + f(prices, dp, i + 1, transactions, 0), 
-                    f(prices, dp, i + 1, transactions, 1)
-                );
-
-        return dp[i][transactions][buy] = Math.max(
-                prices[i] + f(prices, dp, i + 1, transactions - 1, 1), 
-                f(prices, dp, i + 1, transactions, 0)
-            );
-    }
-
     public int maxProfit(int[] prices) {
         int n = prices.length;
+        int[][][] dp = new int[n + 1][3][2];
 
-        int[][][] dp = new int[n + 1][2][2];
-        for(int[][] d : dp)
-            for(int[] p : d)
-                Arrays.fill(p, -1);
+        for(int i = n - 1; i >= 0; i--){
+            for(int transactions = 2; transactions > 0; transactions--){
+                for(int buy = 0; buy < 2; buy++){
+                    if(buy == 1)
+                        dp[i][transactions][buy] = Math.max(-prices[i] + dp[i + 1][transactions][0],  dp[i + 1][transactions][1]);
+                    else
+                        dp[i][transactions][buy] = Math.max(prices[i] + dp[i + 1][transactions - 1][1],  dp[i + 1][transactions][0]);
+                }
+            }
+        }
 
-        return f(prices, dp, 0, 1, 1);
+        return dp[0][2][1];
     }
 }
+
+
+// recursion + memoiation
+
+// class Solution {
+//     private int f(int[] prices, int[][][] dp, int i, int transactions, int buy){
+//         if(transactions == -1 || i == prices.length)
+//             return 0;
+
+//         if(dp[i][transactions][buy] != -1)
+//             return dp[i][transactions][buy];
+
+//         if(buy == 1)
+//             return dp[i][transactions][buy] = Math.max(
+//                     -prices[i] + f(prices, dp, i + 1, transactions, 0), 
+//                     f(prices, dp, i + 1, transactions, 1)
+//                 );
+
+//         return dp[i][transactions][buy] = Math.max(
+//                 prices[i] + f(prices, dp, i + 1, transactions - 1, 1), 
+//                 f(prices, dp, i + 1, transactions, 0)
+//             );
+//     }
+
+//     public int maxProfit(int[] prices) {
+//         int n = prices.length;
+
+//         int[][][] dp = new int[n + 1][2][2];
+//         for(int[][] d : dp)
+//             for(int[] p : d)
+//                 Arrays.fill(p, -1);
+
+//         return f(prices, dp, 0, 1, 1);
+//     }
+// }
