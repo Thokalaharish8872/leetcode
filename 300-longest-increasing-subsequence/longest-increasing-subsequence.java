58967db0c74aa8fe7df3c535f824ddba1b06bc54
Harish Thokala <159756302+Thokalaharish8872@users.noreply.github.com>
2026-08-09 14:51:56 +0530

    Time: 48 ms (36.38%) | Memory: 46.3 MB (43.72%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.java
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.java
@@ -2,44 +2,23 @@
 // maintaining prev greater value 
 
 class Solution {
-    private int f(int[] prices, int[][] dp, int prev, int i){
-        if(i == -1)
-            return 0;
-        
-        if(dp[i][prev] != -1)
-            return dp[i][prev];
-
-        int notPick = f(prices, dp, prev, i - 1);
-
-        int pick = 0;
-        if(prev == prices.length || prices[i] < prices[prev])
-            pick = 1 + f(prices, dp, i, i - 1);
-        
-        return dp[i][prev] = Math.max(pick, notPick);
-    }
-
     public int lengthOfLIS(int[] nums) {
         int n = nums.length;
 
-        int[][] dp = new int[n][n + 1];
+        int[] dp = new int[n];
+        int max = 0;
 
-        for (int prev = 1; prev <= n; prev++) {
-            if (prev == n || nums[0] < nums[prev])
-                dp[0][prev] = 1;
-        }
+        for(int i = 0; i < n; i++){
+            for(int prev = 0; prev < i; prev++){
+                if(nums[prev] < nums[i]){
+                    dp[i] = Math.max(dp[i], 1 + dp[prev]);
 
-        for(int i = 1; i < n; i++){
-            for(int prev = i; prev <= n; prev++){
-                int notPick = dp[i - 1][prev];
-
-                int pick = 0;
-                if(prev == nums.length || nums[i] < nums[prev])
-                    pick = 1 + dp[i - 1][i];
-
-                dp[i][prev] = Math.max(pick, notPick);
+                    max = Math.max(max, dp[i]);
+                }
             }
         }
 
-        return dp[n - 1][n];
+
+        return max + 1;
     }
 }
