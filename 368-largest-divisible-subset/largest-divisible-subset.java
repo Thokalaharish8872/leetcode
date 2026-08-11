fb690c9f4173d330969ba2031a588ccdc5785ac5
Harish Thokala <159756302+Thokalaharish8872@users.noreply.github.com>
2026-08-11 20:58:33 +0530

    Time: 49 ms (5.01%) | Memory: 66.8 MB (5.35%) - LeetSync

--- 368-largest-divisible-subset/largest-divisible-subset.java
+++ 368-largest-divisible-subset/largest-divisible-subset.java
@@ -12,8 +12,6 @@ class Solution {
 
             pick.add(nums[i]);
 
-            System.out.println(pick + "\t" + notPick);
-
             return dp[i][prev] = pick.size() > notPick.size() ? pick : notPick;
         }
         
@@ -24,8 +22,28 @@ class Solution {
         Arrays.sort(nums);
 
         int n = nums.length;
-        List<Integer>[][] dp = new ArrayList[n][n + 1];
+        List<Integer>[][] dp = new ArrayList[n + 1][n + 1];
+        
+        for(int prev = 0; prev <= n; prev++)
+            dp[0][prev] = new ArrayList<>();
 
-        return f(nums, dp, n - 1, n);
+        for(int i = 1; i <= n; i++){
+            for(int prev = 1; prev <= n; prev++){
+                List<Integer> notPick = dp[i - 1][prev];
+                List<Integer> best = notPick;
+
+                if(prev == n || nums[prev] % nums[i - 1] == 0){
+                    List<Integer> pick = new ArrayList<>(dp[i - 1][i - 1]);
+                    pick.add(nums[i - 1]);
+
+                    if(pick.size() > best.size())
+                        best = pick;
+                }
+
+                dp[i][prev] = best;
+            }
+        }
+
+        return dp[n][n];
     }
 }
