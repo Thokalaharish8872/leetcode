072d16e44a6409d0fcf43a60dcf4af0f26231d2b
Harish Thokala <159756302+Thokalaharish8872@users.noreply.github.com>
2026-09-06 11:54:08 +0530

    Time: 23 ms (19.32%) | Memory: 54.7 MB (18.57%) - LeetSync

--- 115-distinct-subsequences/distinct-subsequences.java
+++ 115-distinct-subsequences/distinct-subsequences.java
@@ -1,85 +1,29 @@
 class Solution {
-    // tabulation with space optimization
-      public int numDistinct(String s, String t) {
-        int m = s.length();
-        int n = t.length();
+    private int f(String s, String t, int[][] dp, int i, int j){
+        if(j == t.length())
+            return 1;
 
-        int[] prev = new int[n + 1];
+        if(i == s.length())
+            return 0;
 
-        prev[0] = 1;
-        int topLeft = 1;
+        if(dp[i][j] != -1)
+            return dp[i][j];
 
-        for(int i = 1; i <= m; i++){
-            for(int j = n; j >= 1; j--){
-                int notPick = prev[j];
+        int notPick = f(s, t, dp, i + 1, j);
+        int pick = 0;
 
-                int pick = 0;
-                if(s.charAt(i - 1) == t.charAt(j - 1))
-                    pick = prev[j - 1];
+        if(s.charAt(i) == t.charAt(j))
+            pick = f(s, t, dp, i + 1, j + 1);
 
-                prev[j] = pick + notPick;
-            }
-        }
+        return dp[i][j] = pick + notPick;
+    }
 
-        return prev[n];
+    public int numDistinct(String s, String t) {
+
+        int[][] dp = new int[s.length()][t.length()];
+        for(int[] d : dp)
+            Arrays.fill(d, -1);
+
+        return f(s, t, dp, 0, 0);
     }
 }
-
-    // tabulation 
-
-//     public int numDistinct(String s, String t) {
-//         int m = s.length();
-//         int n = t.length();
-
-//         int[][] dp = new int[m + 1][n + 1];
-//         for(int i = 0; i <= m; i++)
-//             dp[i][0] = 1;
-
-//         for(int i = 1; i <= m; i++){
-//             for(int j = 1; j <= n; j++){
-//                 int notPick = dp[i - 1][j];
-
-//                 int pick = 0;
-//                 if(s.charAt(i - 1) == t.charAt(j - 1))
-//                     pick = dp[i - 1][j - 1];
-
-//                 dp[i][j] = pick + notPick;
-//             }
-//         }
-
-//         return dp[m][n];
-//     }
-// }
-
-    // Recursion with memoiation
-
-    // private int f(String s, String t, int[][] dp, String str, int i, int j){
-    //     if(j == -1)
-    //         return 1;
-
-    //     if(i == -1)
-    //         return 0;
-            
-    //     if(dp[i][j] != -1)
-    //         return dp[i][j];
-
-    //     int notPick = f(s, t, dp, str, i - 1, j);
-
-    //     int pick = 0;
-    //     if(s.charAt(i) == t.charAt(j))
-    //         pick = f(s, t, dp, s.charAt(i) + str, i - 1, j - 1);
-        
-    //     return dp[i][j] = pick + notPick;
-    // }
-
-    // public int numDistinct(String s, String t) {
-    //     int m = s.length();
-    //     int n = t.length();
-
-    //     int[][] dp = new int[m][n];
-    //     for(int[] d : dp)
-    //         Arrays.fill(d, -1);
-
-    //     return f(s, t, dp, "", m - 1, n - 1);
-    // }
-// }
